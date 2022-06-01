a4e4a7f7898f471304408069eb0f85861cc63f6e
vika <ramzelkin@gmail.com>
2022-06-01 15:21:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,31 +3,34 @@ const wsServer = new WebSocket.Server({port: 9000});
 
 let currencies;
 
-  wsServer.on('connection', onConnect);
-  function onConnect(wsClient) {
-    console.log('connect');
-    // wsClient.send('Привет');
-    wsClient.on('message', function(message) {
-        if (message.type === "check") {
-          console.log(currencies);
+const request = require('request');
 
-        }
-        console.log(message);
+request('https://www.nbrb.by/api/exrates/rates?periodicity=0', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  console.log(body);
+  console.log(res.json(JSON.parse(body)));
+  currencies = body;
+});
 
-        wsClient.send(currencies);
-    });
-    wsClient.on('close', function() {
-        console.log('Пользователь отключился');
-    });
-  }
+wsServer.on('connection', onConnect);
+function onConnect(wsClient) {
+  console.log('connect');
+  // wsClient.send('Привет');
+  wsClient.on('message', function(message) {
+      if (message.type === "check") {
+        console.log(currencies);
 
+      }
+      console.log(message);
 
-
-  const request = require('request');
-
-  request('https://www.nbrb.by/api/exrates/currencies', { json: true }, (err, res, body) => {
-    if (err) { return console.log(err); }
-    
-    currencies = body;
+      wsClient.send(currencies);
   });
+  wsClient.on('close', function() {
+      console.log('Пользователь отключился');
+  });
+}
+
+
+
+ 
     
